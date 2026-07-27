a7032cf181a118ddb208727213ce22d6b5c938e8
ankitasGIT <ankitalonkar101@gmail.com>
2026-07-27 19:39:58 +0530

    mega project appwrite setup-config added

---
+++ MegaProject/Steps.md
@@ -0,0 +1,17 @@
+1. .env & Appwrite - Done
+
+Added .env 
+Added conf.js
+Config of appwrite done
+
+
+2. Build auth service with appwrite - DONE
+
+New custom authService - auth.js
+
+
+3. Appwrite database, file upload, custom queries - DONE
+
+Added config.js - Created class for custom db, file operations
+
+4. Configure redux toolkit - 
---
+++ MegaProject/src/appwrite/auth/config.js
@@ -0,0 +1,131 @@
+import conf from "../../conf/conf.js"
+import { Client, Account, Databases, Storage, Query, ID } from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client 
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client)
+    } 
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                sluf,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                sluf,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                sluf
+            )
+            return true;
+        } catch (error) {
+            throw error;
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            throw error;
+            return false;
+        }
+    }
+
+    // Query
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            throw error;
+            return false;
+        }
+    }
+
+    //File upload
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            throw error;
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+}
+
+const service = new Service();
+
+export default service;
